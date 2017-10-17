7ef1acdda62601599c188cae2e8b992a967a79b1
Dmitry Tsydenov <rondimts@gmail.com>
2017-10-18 06:07:44 +0700

    Added graphcool schema

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj8p2ygkq01250163gfr11kr3'
+});
+const client = new ApolloClient({ networkInterface });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'));
 registerServiceWorker();
